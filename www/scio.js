23e97dc1a5946cc6fb2b045d85b23cb3b00260ff
Shadowstep33 <gabriel@squarevault.com>
2016-10-03 16:01:17 -0500

    Update scio.js

--- www/scio.js
+++ www/scio.js
@@ -1,38 +1,16 @@
-/**************** Scio Device JS Interface ***********************
-
-- pair
-- connect
-- scan
-- calibrate
-- checkCalibration
-
-**********************************************************/
-function scioDevice(){
-
-}
-scioDevice.prototype.constructor = scioDevice;
-
-scioDevice.prototype.connect = function(){
-	cordova.exec(
-		function(winParam) {
-			alert("Scio Connected");
-		},
-		function(error) {
-			alert(error + " Scio not connected");
-		},
-		"ScioDevice",
-		"connect",
-		[]
-	);
-};
-
-
-/**************** Scio Cloud JS Interface ***********************
-
-- analyze
-
-**********************************************************/
-
+/*global cordova*/
 module.exports = {
-	connect: scioDevice.prototype.connect
+	connect: function(){
+		cordova.exec(
+			function(winParam) {
+				alert("Scio Connected");
+			},
+			function(error) {
+				alert(error + " Scio not connected");
+			},
+			"ScioDevice",
+			"connect",
+			[]
+		);
+	}
 };
