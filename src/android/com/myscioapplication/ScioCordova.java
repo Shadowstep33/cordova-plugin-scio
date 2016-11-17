22a433feb223209a65e32da71d53b26ccb156a7a
Shadowstep33 <godavid33@gmail.com>
2016-11-17 17:28:11 -0600

    turn bool to string for isConnected

--- src/android/com/myscioapplication/ScioCordova.java
+++ src/android/com/myscioapplication/ScioCordova.java
@@ -315,6 +315,323 @@ public class ScioCordova extends CordovaPlugin implements IScioDevice {
 					}
 					
 					callbackContext.success(jsonArray.toString());
+				}package com.squarevault.cordova.scio;
+
+import org.apache.cordova.CordovaPlugin;
+import org.apache.cordova.CallbackContext;
+import org.apache.cordova.PluginResult;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
+import android.os.Bundle;
+import android.view.WindowManager;
+import android.widget.ArrayAdapter;
+import android.widget.Toast;
+
+import com.consumerphysics.android.scioconnection.services.SCiOBLeService;
+import com.consumerphysics.android.scioconnection.utils.BLEUtils;
+import com.consumerphysics.android.sdk.callback.device.ScioDeviceCallback;
+import com.consumerphysics.android.sdk.callback.device.ScioDeviceConnectHandler;
+import com.consumerphysics.android.sdk.sciosdk.ScioCloud;
+import com.consumerphysics.android.sdk.sciosdk.ScioDevice;
+import com.consumerphysics.android.sdk.callback.cloud.ScioCloudModelsCallback;
+import com.consumerphysics.android.sdk.callback.cloud.ScioCloudCPModelsCallback;
+import com.consumerphysics.android.sdk.callback.cloud.ScioCloudAnalyzeManyCallback;
+import com.consumerphysics.android.sdk.callback.cloud.ScioCloudSCiOVersionCallback;
+import com.consumerphysics.android.sdk.callback.cloud.ScioCloudUserCallback;
+import com.consumerphysics.android.sdk.callback.device.ScioDeviceBatteryHandler;
+import com.consumerphysics.android.sdk.callback.device.ScioDeviceCalibrateHandler;
+import com.consumerphysics.android.sdk.callback.device.ScioDeviceCallbackHandler;
+import com.consumerphysics.android.sdk.callback.device.ScioDeviceScanHandler;
+import com.consumerphysics.android.sdk.model.ScioBattery;
+import com.consumerphysics.android.sdk.model.ScioModel;
+import com.consumerphysics.android.sdk.model.ScioCPModel;
+import com.consumerphysics.android.sdk.model.ScioReading;
+import com.consumerphysics.android.sdk.model.ScioUser;
+import com.consumerphysics.android.sdk.model.attribute.ScioAttribute;
+import com.consumerphysics.android.sdk.model.attribute.ScioNumericAttribute;
+import com.consumerphysics.android.sdk.model.attribute.ScioStringAttribute;
+import com.consumerphysics.android.sdk.model.attribute.ScioDatetimeAttribute;
+import com.consumerphysics.android.sdk.sciosdk.ScioLoginActivity;
+
+import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+
+import consumerphysics.com.myscioapplication.config.Constants;
+import consumerphysics.com.myscioapplication.interfaces.IScioDevice;
+import consumerphysics.com.myscioapplication.utils.StringUtils;
+
+public class ScioCordova extends CordovaPlugin implements IScioDevice {
+
+    private final static int LOGIN_ACTIVITY_RESULT = 1000;
+    // TODO: Put your redirect url here!
+    private static final String REDIRECT_URL = "http://monsterisland.chimeracompanygames.com";
+
+    // TODO: Put your app key here!
+    private static final String APPLICATION_KEY = "636f2a8e-ae74-41b1-92dd-c79766ce804c";
+	
+    // Members
+    private String deviceName;
+    private String deviceAddress;
+    private String username;
+    private String modelId;
+    private int modelIndex;
+    private String modelName;
+	private ScioModel model;
+	private JSONArray last_args;
+	
+    private Map<String, String> devices;
+    private DevicesAdapter devicesAdapter;
+    private BluetoothAdapter bluetoothAdapter;
+
+    // BlueTooth scan callback
+    private BluetoothAdapter.LeScanCallback leScanCallback = new BluetoothAdapter.LeScanCallback() {
+        @Override
+        public void onLeScan(BluetoothDevice device, int rssi, byte[] scanRecord) {
+            String deviceName = device.getName();
+
+            // Show only SCiO devices
+            if (deviceName != null && deviceName.startsWith("SCiO")) {
+                deviceName = deviceName.substring(4);
+                String scio = devices.get(device.getAddress());
+				
+                final Device dev = new Device(device.getAddress(), deviceName);
+                storeDevice(dev);
+                Toast.makeText(context, dev.getName() + " was selected", Toast.LENGTH_SHORT).show();
+				// Stop Scan
+				bluetoothAdapter.stopLeScan(leScanCallback);
+				callbackContext.success("Scio "+deviceName+ " found with BLE");
+                // if (scio == null) {
+                    // addDevice(deviceName, device.getAddress());
+                // }
+            }
+        }
+    };
+	
+	private Context context;
+	private Activity mActivity;
+	private CallbackContext callbackContext;
+    private ScioCloud scioCloud;
+    private ScioDevice scioDevice;
+
+    protected ScioCloud getScioCloud() {
+        return scioCloud;
+    }
+
+    protected ScioDevice getScioDevice() {
+        return scioDevice;
+    }
+	
+    protected boolean isDeviceConnected() {
+        return scioDevice != null && scioDevice.isConnected();
+    }
+
+    protected boolean isLoggedIn() {
+        return scioCloud != null && scioCloud.hasAccessToken();
+    }
+
+    protected SharedPreferences getSharedPrefs() {
+        return context.getSharedPreferences(Constants.PREF_FILE, Context.MODE_PRIVATE);
+    }
+	
+    private final class Device {
+        private String address;
+        private String name;
+
+        public String getName() {
+            return name;
+        }
+
+        public String getAddress() {
+            return address;
+        }
+
+        public Device(final String address, final String name) {
+            this.name = name;
+            this.address = address;
+        }
+    }
+	
+    public class DevicesAdapter extends ArrayAdapter<Device> {
+        public DevicesAdapter(final Context context, final List<Device> devices) {
+            super(context, 0, devices);
+        }
+    }
+
+    @Override
+    public boolean execute(String action, JSONArray args, CallbackContext cbCtx) throws JSONException {
+		context = this.cordova.getActivity().getApplicationContext();
+		mActivity = this.cordova.getActivity();
+		callbackContext = cbCtx;
+		last_args = args;
+		
+        if (action.equals("connect")) {
+			/* Get the Preferred Scio Device and Connect */
+			final String deviceAddress = getSharedPrefs().getString(Constants.SCIO_ADDRESS, null);
+			
+			if (!StringUtils.isEmpty(deviceAddress)) {
+				connect(deviceAddress);
+			}else{
+				callbackContext.error("Device ID Empty");
+			}
+
+            return true;
+        }
+		
+        if (action.equals("scanble")) {
+
+			devices = new LinkedHashMap<>();
+
+			final List<Device> arrayOfDevices = new ArrayList<>();
+			devicesAdapter = new DevicesAdapter(context, arrayOfDevices);
+
+			bluetoothAdapter = BLEUtils.getBluetoothAdapter(context);
+
+			// Start Scan
+			bluetoothAdapter.startLeScan(leScanCallback);
+            return true;
+        }
+		
+        if (action.equals("scan")) {
+
+			doScan();
+            return true;
+        }
+		
+        if (action.equals("calibrate")) {
+
+			doCalibrate();
+            return true;
+        }
+		
+		if(action.equals("login")){
+		
+			doLogin();
+			return true;
+		}
+		
+		if(action.equals("logout")){
+		
+			doLogout();
+			return true;
+		}
+		
+		if(action.equals("setmodel")){
+			getScioCloud().getModels(new ScioCloudModelsCallback() {
+				@Override
+				public void onSuccess(List<ScioModel> models) {
+					storeSelectedModels(models);
+					
+					for (int i = 0; i < models.size(); i++) {
+						try{
+							if(models.get(i).getName().equals(last_args.getString(0))){
+								model = models.get(i);
+								modelIndex = i;
+							}
+						}catch(JSONException e){
+						
+						}
+					}
+					
+					callbackContext.success("Set Model To: "+model.getName());
+				}
+
+				@Override
+				public void onError(int code, String msg) {
+					Toast.makeText(context, "Error while retrieving models", Toast.LENGTH_SHORT).show();
+					callbackContext.error(msg);
+				}
+			});	
+			
+			return true;
+		}
+		
+		if(action.equals("setmodels")){
+			SharedPreferences pref = context.getSharedPreferences(Constants.PREF_FILE, Context.MODE_PRIVATE);
+			String mIDs = pref.getString(Constants.MODEL_ID, null);
+			List<String> input = Arrays.asList(last_args.getString(0).split(","));
+			List<String> mNames = Arrays.asList(mIDs.split(","));
+			
+			modelId = "";
+			for (int i = 0; i < input.size(); i++) {
+				if(i > 0)
+					modelId = modelId + ",";
+				
+				modelId = modelId + mNames.get(Integer.parseInt(input.get(i)));
+			}
+			
+			callbackContext.success("Set Model To: "+modelId);
+
+			return true;
+		}
+		
+		if(action.equals("getmodels")){
+
+			getScioCloud().getModels(new ScioCloudModelsCallback() {
+				@Override
+				public void onSuccess(List<ScioModel> models) {
+					storeSelectedModels(models);
+					
+					//Iterate models and push to JSON to send to cordova
+					JSONArray jsonArray = new JSONArray();
+					
+					for (int i = 0; i < models.size(); i++) {
+						JSONObject item = new JSONObject();
+						
+						try{
+							item.put("name", models.get(i).getName());
+							item.put("index", i);					
+							jsonArray.put(item);
+						}catch(JSONException e){
+						
+						}
+					}
+					
+					callbackContext.success(jsonArray.toString());
+				}
+
+				@Override
+				public void onError(int code, String msg) {
+					Toast.makeText(context, "Error while retrieving models", Toast.LENGTH_SHORT).show();
+					callbackContext.error(msg);
+				}
+			});		
+            return true;
+		}
+		
+		if(action.equals("getcpmodels")){
+
+			getScioCloud().getCPModels(new ScioCloudCPModelsCallback() {
+				@Override
+				public void onSuccess(List<ScioCPModel> models) {
+					storeSelectedCPModels(models);
+					
+					//Iterate models and push to JSON to send to cordova
+					JSONArray jsonArray = new JSONArray();
+					
+					for (int i = 0; i < models.size(); i++) {
+						JSONObject item = new JSONObject();
+						
+						try{
+							item.put("name", models.get(i).getName());
+							item.put("index", i);					
+							jsonArray.put(item);
+						}catch(JSONException e){
+						
+						}
+					}
+					
+					callbackContext.success(jsonArray.toString());
 				}
 
 				@Override
@@ -327,7 +644,7 @@ public class ScioCordova extends CordovaPlugin implements IScioDevice {
 		}
 		if(action.equals("isconnected")){
 
-			callbackContext.success(isDeviceConnected());
+			callbackContext.success(valueOf(isDeviceConnected()));
             return true;
 		}
 		
@@ -709,4 +1026,8 @@ public class ScioCordova extends CordovaPlugin implements IScioDevice {
     private boolean isNull(final String value) {
         return value == null || value.equals("null");
     }
+	
+	public static String valueOf(Object obj) {
+		return (obj == null) ? "null" : obj.toString();
+	}	
 }
